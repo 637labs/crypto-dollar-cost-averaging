61f0483199b918421a62a8454d7bed4f1ced028c
Nico Rakover <nrakover@gmail.com>
2021-05-16 23:48:40 -0700

    try again: use response.data as token

--- frontend/api.ts
+++ frontend/api.ts
@@ -5,30 +5,23 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 const API_URL = process.env.API_URL;
 const API_TOKEN_URL = `http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=${API_URL}`;
 
-interface AccessToken {
-    access_token: string;
-    token_type: 'Bearer';
-    expires_in: number;
-}
 class ApiService {
     static authenticatedRequest(config: AxiosRequestConfig, onSuccess: (value: AxiosResponse<any>) => void, onError: (reason: any) => void) {
         if (process.env.NODE_ENV != 'development') {
-            axios.get<AccessToken>(API_TOKEN_URL,
-                {
-                    method: 'get',
-                    headers: {
-                        'Metadata-Flavor': 'Google'
-                    }
+            axios({
+                method: 'get',
+                url: API_TOKEN_URL,
+                headers: {
+                    'Metadata-Flavor': 'Google'
                 }
-            ).then((tokenResponse) => {
-                const accessToken = tokenResponse.data.access_token;
+            }).then((token) => {
                 const { headers } = config;
                 return axios({
                     ...config,
                     baseURL: API_URL,
                     headers: {
                         ...headers,
-                        'Authorization': `Bearer ${accessToken}`
+                        'Authorization': `Bearer ${token.data}`
                     }
                 });
             }).then((value) => {
