cc5106d5b3c34a9be90dca3be9dd14cc13e543b8
Peter Argany <peter_argany2@hotmail.com>
2022-06-25 21:05:08 -0700

    Clean up horizontal navigation

--- client/src/shuffle/pages/Index.tsx
+++ client/src/shuffle/pages/Index.tsx
@@ -7,23 +7,14 @@ import Footer1 from '../components/Footer1';
 
 export default function Index(): JSX.Element {
   const navigationContent = {
-    brand: { text: 'Nckl' },
-    link1: 'Section One',
-    link2: 'Section Two',
-    link3: 'Section Three',
-    link4: 'Section Four',
-    link5: 'Section Five',
-    primaryAction: 'Action',
+    brand: { text: 'nckl' },
   };
 
   return (
     <React.Fragment>
       <HorizontalNav3 content={navigationContent} />
-
       <Features4 content={null} />
-
       <Pricing2 content={null} />
-
       <Footer1 content={null} />
     </React.Fragment>
   );
